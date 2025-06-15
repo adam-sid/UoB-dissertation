ca0341b1f5ca430e8dead9f9fb37fb19b5cb688d
adam-sid <148649495+adam-sid@users.noreply.github.com>
2025-06-15 18:04:14 +0100

    add initial formatting

--- move_pdf.sh
+++ move_pdf.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 SOURCE_DIR="build"
-FILENAME="main.pdf"
+FILENAME="msc_dissertation.pdf"
 
 if [ -f "$SOURCE_DIR/$FILENAME" ]; then
     mv "$SOURCE_DIR/$FILENAME" "./"
-    mv "$FILENAME" "msc_project_readable.pdf"
+    mv "$FILENAME" "msc_disseration_readable.pdf"
 else
     echo "Error: $SOURCE_DIR/$FILENAME not found. Did the build succeed?"
 fi
